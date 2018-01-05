4e41d3698924c631ae112d4ad952b59a1c5e5b96
sergavv14 <sergavv14@gmail.com>
2018-01-05 20:27:12 +0200

    Пройден тест №3.(transferMoneyOnWorkingdaysOnNationalHolidays()) 1)Пофискил кострукторы и методы FeeService 2)Создал тест №3 (transferMoneyOnWorkingdaysOnNationalHolidays()) и пофиксил ошибки в приложениии.

--- src/main/java/ua/dp/skillsup/tdd/FeeService.java
+++ src/main/java/ua/dp/skillsup/tdd/FeeService.java
@@ -12,32 +12,43 @@ public class FeeService implements InitializingBean{
     public FeeService() {
         fee = 0.01;
         this.weekendService = new WeekendService();
+        this.holidayService = new HolidayService();
     }
 
     public FeeService(HolidayService holidayService) {
+        fee = 0.01;
+        this.weekendService = new WeekendService();
         this.holidayService = holidayService;
     }
 
     public FeeService(WeekendService weekendService) {
         fee = 0.01;
         this.weekendService = weekendService;
+        this.holidayService = new HolidayService();
     }
 
-
-    public void setFee(double fee) {
-        this.fee = fee;
-    }
-
-    public void setHolidayService(HolidayService holidayService) {
+    public FeeService(WeekendService weekendService, HolidayService holidayService){
+        fee = 0.01;
+        this.weekendService = weekendService;
         this.holidayService = holidayService;
     }
 
+    public void setHolidayService(HolidayService holidayService) {this.holidayService = holidayService;}
+
+    public void setWeekendService(WeekendService weekendService) {this.weekendService = weekendService;}
+
+
+    public HolidayService getHolidayService() {return holidayService;}
+
+    public WeekendService getWeekendService() {return weekendService;}
 
     public double getFee(double paymentAmount) {
-        if (weekendService.isWeekend())
-            return fee*1.5;
-
-        return fee;
+        if (weekendService.isWeekend() & holidayService.isHoliday()) {
+            return fee*2;
+        }if(!weekendService.isWeekend() & !holidayService.isHoliday()){
+            return fee;
+        }
+        return fee*1.5;
     }
 
 
--- src/main/java/ua/dp/skillsup/tdd/HolidayService.java
+++ src/main/java/ua/dp/skillsup/tdd/HolidayService.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.Random;
 
 public class HolidayService {
-    public boolean isHoliday(Date date){
+    public boolean isHoliday(){
         Random random = new Random();
         return random.nextDouble() > 0.9;
     }
